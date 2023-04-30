5d0c8032f0045d56404030e4e6a8ac63788bbc0c
Timur Sultanaev <str.write@gmail.com>
2023-04-30 06:54:23 +0200

    check cache

--- src/clone.rs
+++ src/clone.rs
@@ -6,16 +6,12 @@ use fehler::throws;
 #[throws(anyhow::Error)]
 pub(crate) fn clone_repo(repo_url: &str, dst: &PathBuf) {
     println!("Cloning repository");
-    
     let mut cmd = std::process::Command::new("git");
     cmd.arg("clone");
     cmd.arg(repo_url);
     cmd.arg(dst);
-    
     let child = cmd.spawn()?;
-
     let output = child.wait_with_output()?;
-
     if !output.status.success() {
         anyhow::bail!("Failed to clone repository");
     }
